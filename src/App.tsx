93fd0781288d50a9a2ae2caa1102596ced87743a
Shinny22 <134633616+Shinny22@users.noreply.github.com>
2024-05-20 23:21:11 +0100

    [UPDATE] I've done with style on mycontent items

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,18 @@ import Buttonnav from './Component/Buttonnav';
 
 const menu = [
   {
-    image: "public/asset/Group.png",
-    text: "Introduce Your Product Quickly & Effectively",
+    image: "public/asset/Mobile Marketing-pana copy.svg",
+    text: "Light, Fast & Powerful",
     content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim."
   },
   {
-    image: "/asset/Group.png",
-    text: "Introduce Your Product Quickly & Effectively",
+    image: "public/asset/Team goals-pana.svg",
+    text: "Light, Fast & Powerful",
     content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim."
   },
   {
-    image: "/asset/Group.png",
-    text: "Introduce Your Product Quickly & Effectively",
+    image: "public/asset/Mobile Marketing-bro.svg",
+    text: "Light, Fast & Powerful",
     content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim."
   }
 ];
@@ -37,14 +37,14 @@ function App() {
         </div>
 
        
-          <div className='relative ml-20 left-[500px] bottom-[230px] '>
+          <div className='relative ml-20 left-[500px] bottom-[230px]'>
             <Image image="/asset/Group.png" value="Il s'agit d'une image" />
           
         </div>
       
     </div>
       {/* Render the menu items */}
-      <div className='menu-list gap-5 mt-5'>
+      <div className='menu-list gap-2 '>
         {menu.map((item, index) => (
           <Content key={index} image={item.image} text={item.text} content={item.content} />
         ))}
--- src/Component/Content.tsx
+++ src/Component/Content.tsx
@@ -15,14 +15,14 @@ const Content:React.FC<Props>=(props:Props)=>{
     return (
 
         
-            <div className="flex flex-between mb-2 ">
-                <div className="w-150 h-100 ">
+            <div className="flex flex-between mb-2 gap-[75px] pt-10">
+                <div className="w-[350px] h-[300px] gap-5 ml-10 mt-10">
                     <img src={props.image} alt="Menu item" />
                 </div>
 
-                <div className="text-black text-center font-thin w-[50%] pt-"> 
-                    {props.text} 
-                    {props.content} 
+                <div className="text-black text-center font-thin w-[50%] pl-[100px] pt-20 mt-10"> 
+                   <h1 className='text-2xl font-semibold text-[#091133] mb-5'>{props.text} </h1>
+                    <p>{props.content}</p> 
                 </div>
                 
             </div>
